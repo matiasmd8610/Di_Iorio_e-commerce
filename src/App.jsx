d2d4e87e227345fbbb6ee2f3603c2c1bbe2a39eb
Matias Di Iorio <matiasmd8610@gmail.com>
2025-04-12 14:13:34 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ItemListContainer from './components/ItemListContainer'
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <>
+      {/* <h1>Vite + React</h1> */}
+      <NavBar />
+      <ItemListContainer />
+    </>
+  )
+}
+
+export default App
